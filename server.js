2224462182a527e1fdacf008199433e3213e57a2
Danika Harada <danimi65@gmail.com>
2017-03-21 23:58:38 -1000

    started on react

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({
   extended:true
 }));
 
+app.use(express.static('public'));
 app.use(bodyParser.json({
   extended:true
 }));
---
+++ src/Containers/Board.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Board extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      todoCard: [],
+      currentCard: [],
+      completedCard: []
+    }
+  }
+  render(){
+    return(
+    )
+  }
+}
+
+export default Board;
